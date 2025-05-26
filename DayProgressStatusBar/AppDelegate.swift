aee63366760cfa974deba489da6e4a4fa863efd7
parkyohan <pyhan23@gmail.com>
2025-05-26 10:08:06 +0900

    Initial Commit

---
+++ DayProgressStatusBar/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  DayProgressStatusBar
+//
+//  Created by 박요한 on 5/26/25.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ DayProgressStatusBar/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  DayProgressStatusBar
+//
+//  Created by 박요한 on 5/26/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
